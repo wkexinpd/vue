27090c768f516b99b50b21909a8741f56458a185
wkexin <760568219@qq.com>
2019-11-29 21:10:50 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <nav-bar></nav-bar>
+    <div class="content-box">
+
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from './components/NavBar.vue'
+import Footer from './components/footer.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+    NavBar
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//全局配置 关闭产品控制台提示
 Vue.config.productionTip = false
 
+//创建当前项目运行的根实例组件
 new Vue({
   render: h => h(App),
 }).$mount('#app')
